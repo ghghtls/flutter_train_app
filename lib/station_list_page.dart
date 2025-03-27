24efcb3034ceecf3fea42f37c9bad3002e0b2bfa
ghghtls <ghghtls2@gmail.com>
2025-03-27 22:01:51 +0900

    feat:리스트페이지 뒤로가기버튼

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 class StationListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('StationListPage');
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // 뒤로가기
+          },
+        ),
+        title: Text('출발역'),
+      ),
+    );
   }
 }
