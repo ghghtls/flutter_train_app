5116ba7f02400fd4d24f05362869171056d37a7b
ghghtls <ghghtls2@gmail.com>
2025-03-28 13:53:00 +0900

    feat:list페이지 디자인 완료

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -14,6 +14,9 @@ class StationListPage extends StatelessWidget {
   // '경주',
   // '울산',
   // '부산',
+  ///final 값을 한번만 설정하고 변경하지 못하게,생성자에서 한번만 설정 가능하고 이후에는 값 변경 안됨 , 읽기전용 빌드
+  ///required 생성할때 반드시 값을 넣어야 한다는 뜻  둘다 같이 쓰면 한번만 지정되고,절대 변경 되지않음 안정성+명확성 모두 확보
+
   StationListPage({required this.stations, required this.type});
   @override
   Widget build(BuildContext context) {
@@ -31,11 +34,32 @@ class StationListPage extends StatelessWidget {
           },
         ),
         title: Text(type),
+
+        ///홈페이지에 설정한 값 설정
       ),
       body: ListView.builder(
         itemCount: stationList.length,
         itemBuilder: (context, index) {
-          return ListTile(title: Text(stationList[index]));
+          return Column(
+            children: [
+              ListTile(
+                title: Container(
+                  height: 50,
+                  alignment: Alignment.centerLeft, // 가로는 왼쪽, 수직은 가운데
+
+                  child: Text(
+                    stationList[index],
+
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pop(context, stationList[index]);
+                },
+              ),
+              Container(height: 1, color: Colors.grey[300]!),
+            ],
+          );
         },
       ),
     );
