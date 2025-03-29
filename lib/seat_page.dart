26d5bcbbed5fda30a3cc64dc0c3f5443daea0853
ghghtls <ghghtls2@gmail.com>
2025-03-29 23:48:20 +0900

    feat:시트페이지 디자인 두번째 구현 ,좌석 눌렀을때 색깔 만드는 기능 ,선택됨 컨테이너 구현

--- lib/seat_page.dart
+++ lib/seat_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SeatPage extends StatelessWidget {
+class SeatPage extends StatefulWidget {
   SeatPage(
     this.selectedRow,
     this.selectedCol,
@@ -15,10 +15,24 @@ class SeatPage extends StatelessWidget {
   final String selectedArrive;
   final void Function(int row, int col) onSelected;
 
-  final int totalRows = 20; // 전체 좌석 행수
+  @override
+  State<SeatPage> createState() => _SeatPageState();
+}
+
+class _SeatPageState extends State<SeatPage> {
+  int? _selectedRow;
+  int? _selectedCol;
+
+  final int totalRows = 20;
+  // 전체 좌석 행수
   final int seatsPerRow = 4;
 
+  ///열은 총 4개
+
+  final List<String> ColumnLabels = ['A', 'B', 'C', 'D'];
+
   ///각 행에 좌석 수
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,10 +59,9 @@ class SeatPage extends StatelessWidget {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
-
                   children: [
                     Text(
-                      selectedDeparture,
+                      widget.selectedDeparture,
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.purple,
@@ -59,20 +72,66 @@ class SeatPage extends StatelessWidget {
                     Icon(Icons.arrow_circle_right_outlined, size: 30),
                     SizedBox(width: 30),
                     Text(
-                      selectedArrive,
+                      widget.selectedArrive,
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.purple,
                         fontSize: 30,
                       ),
-                    ), // 위젯들
+                    ),
                   ],
                 ),
+              ),
+
+              SizedBox(height: 16),
+
+              ///텍스트와 범례 사이 간격
+
+              /// 범례를 한 줄 Row로 구성 (중첩 구조 제거)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  SizedBox(width: 4),
+                  Text('선택됨', style: TextStyle(fontSize: 18)),
+
+                  SizedBox(width: 20),
+
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300]!,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  SizedBox(width: 4),
+                  Text('선택안됨', style: TextStyle(fontSize: 18)),
+                ],
               ),
 
               SizedBox(height: 30),
 
               ///텍스트와 좌석 간격
+
+              ///열 라벨 표시 (A,B [통로] C,D)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildColumnLabel('A'),
+                  _buildColumnLabel('B'),
+                  SizedBox(width: 68),
+                  _buildColumnLabel('C'),
+                  _buildColumnLabel('D'),
+                ],
+              ),
 
               ///좌석 생성 부분
               Column(
@@ -83,14 +142,37 @@ class SeatPage extends StatelessWidget {
                     ///세로 간격 8
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(seatsPerRow, (colIndex) {
-                        return Padding(
-                          padding: const EdgeInsets.only(right: 4.0),
+                      children: [
+                        /// 왼쪽 좌석 A,B
+                        Seat(rowIndex, 0),
+                        SizedBox(width: 4),
 
-                          ///가로 간격 4
-                          child: Seat(rowIndex, colIndex),
-                        );
-                      }),
+                        ///가로간격 4
+
+                        ///좌석 사이 간격
+                        Seat(rowIndex, 1),
+
+                        ///가운데 통로 + 행번호
+                        SizedBox(width: 30),
+
+                        SizedBox(
+                          width: 30,
+                          child: Center(
+                            child: Text(
+                              '${rowIndex + 1}',
+
+                              ///중앙에 행 번호 출력
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 30),
+
+                        ///오른쪽 좌석 C,D
+                        Seat(rowIndex, 2),
+                        SizedBox(width: 4),
+                        Seat(rowIndex, 3),
+                      ],
                     ),
                   );
                 }),
@@ -102,9 +184,34 @@ class SeatPage extends StatelessWidget {
     );
   }
 
+  ///열라벨 위젯 (A~D)
+  Widget _buildColumnLabel(String label) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Container(
+        width: 50,
+        alignment: Alignment.center,
+        child: Text(
+          label,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget Seat(int rowNum, int colNum) {
     return GestureDetector(
-      onTap: () => onSelected(rowNum, colNum),
+      onTap: () {
+        setState(() {
+          _selectedRow = rowNum;
+          _selectedCol = colNum;
+        });
+        widget.onSelected(rowNum, colNum);
+      },
 
       ///클릭 시 이벤트 호출
       child: Container(
@@ -114,7 +221,7 @@ class SeatPage extends StatelessWidget {
         height: 50,
         decoration: BoxDecoration(
           color:
-              rowNum == selectedRow && colNum == selectedCol
+              rowNum == _selectedRow && colNum == _selectedCol
                   ? Colors.purple
                   : Colors.grey[300]!,
           borderRadius: BorderRadius.circular(20),
